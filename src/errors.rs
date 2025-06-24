1decfec7dd7fd804af7cc8df28d04037c2a8148f
Nitant2727 <laherunitant04@proton.me>
2025-06-24 07:38:29 +0000

    Basic setup for logging and error handling

---
+++ src/errors.rs
@@ -0,0 +1,38 @@
+use std::fmt;
+use std::process::exit;
+
+#[derive(Debug)]
+pub enum Errcode {
+    ArguementInvalid(&'static str),
+}
+
+impl Errcode {
+    pub fn get_retcode(&self) -> i32 {
+        1
+    }
+}
+
+#[allow(unreachable_patterns)]
+impl fmt::Display for Errcode {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match &self {
+            Errcode::ArguementInvalid(element) => write!(f, "Arguement Invalid : {}", element),
+            _ => write!(f, "{:?}", self),
+        }
+    }
+}
+
+pub fn exit_with_retcode(res: Result<(), Errcode>) {
+    match res {
+        Ok(_) => {
+            log::debug!("Exiting without any errors, returning 0");
+            exit(0);
+        }
+
+        Err(e) => {
+            let retcode = e.get_retcode();
+            log::error!("Error on exit\n\t{}\n\tReturning {}", e, retcode);
+            exit(retcode);
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+mod cli;
+use std::process::exit;
+mod errors;
+use errors::Errcode;
+
+use crate::errors::exit_with_retcode;
+
 fn main() {
-    println!("Hello, world!");
+    match cli::parse_args() {
+        Ok(args) => {
+            log::info!("{:?}", args);
+            exit_with_retcode(Ok(()))
+        }
+
+        Err(e) => {
+            log::error!("Error while parsing arguements : \n \t {}", e);
+            exit(e.get_retcode());
+        }
+    }
 }
